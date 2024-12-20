63edba6b9af5d1b176bd13054f1165c384332c47
Eduardo <eduardolunardi942@gmail.com>
2024-12-20 18:33:17 -0300

    build: font size and colors

--- App.tsx
+++ App.tsx
@@ -1,16 +1,20 @@
 import React from 'react'
 import { StatusBar } from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { ThemeProvider } from 'styled-components'
 
 import { Navigation } from './src/navigation'
+import theme from './theme'
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <StatusBar barStyle={'dark-content'} backgroundColor={'#FFFFFF'} />
-        <Navigation />
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <ThemeProvider theme={theme}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <StatusBar barStyle={'dark-content'} backgroundColor={'#FFFFFF'} />
+          <Navigation />
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </ThemeProvider>
   )
 }
---
+++ theme.ts
@@ -0,0 +1,18 @@
+const theme = {
+  colors: {
+    gray: '#d9d9d9',
+    blue: '#425a76',
+    blue100: '#67b6fa',
+    orange: '#f7933a',
+    green: '#83d352',
+    red: '#ce3333',
+    white: '#ffffff',
+    black: '#000000'
+  },
+  fontSize: {
+    small: '13px',
+    large: '24px'
+  }
+}
+
+export default theme
