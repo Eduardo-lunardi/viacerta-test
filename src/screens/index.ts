16e9f97cb62ec1d49b57019da7713f38b0b8e777
Eduardo <eduardolunardi942@gmail.com>
2024-12-26 11:11:05 -0300

    fix: creat to create

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,3 @@
-export { default as CreateAccount } from './creat-account'
+export { default as CreateAccount } from './create-account'
 export { default as Home } from './Home'
 export { default as Success } from './Success'
